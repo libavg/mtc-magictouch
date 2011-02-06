4e056954fe5f06c799234974f2b24983ad340956
BenediktSeidl <Benedikt.Seidl@gmx.de>
2011-02-06 17:12:08 +0000

    /avg_media/mtc/magictouch/: typos + deleted unnecessary files
    svn path=/trunk/avg_media/mtc/magictouch/; revision=5504

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 from distutils.core import setup
 
 setup(
-    name='magicotuch',
+    name='magictouch',
     version='0.1',
     author='Benedikt Seidl',
     author_email='Benedikt.Seidl@gmx.de',
     url='https://www.libavg.de/svn/trunk/avg_media/mtc/magictouch/',
     license='GNU GPL',
-    packages=['magicotuch'],
+    packages=['magictouch'],
     scripts=['scripts/magictouch'],
     package_data={
-            'magicotuch': ['media/*.png', 'fonts/*.ttf'],
+            'magictouch': ['media/*.png', 'fonts/*.ttf'],
     }
 )
 
